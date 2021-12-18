08a5d86c80fa97c202f1b1668f72e305bc20a466
Suleman Khan <sulemank97@gmail.com>
2021-12-18 20:16:08 +0500

    setup users service for users CRUD

---
+++ backend/src/models/users.model.ts
@@ -0,0 +1,41 @@
+// See https://sequelize.org/master/manual/model-basics.html
+// for more of what you can do here.
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Application } from '../declarations';
+import { HookReturn } from 'sequelize/types/lib/hooks';
+
+export default function (app: Application): typeof Model {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient');
+  const users = sequelizeClient.define(
+    'users',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      profilePhoto: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      contactInformation: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      hooks: {
+        beforeCount(options: any): HookReturn {
+          options.raw = true;
+        },
+      },
+    }
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (users as any).associate = function (models: any): void {
+    // Define associations here
+    // See https://sequelize.org/master/manual/assocs.html
+  };
+
+  return users;
+}
