2371cb6d7dc20c58b72e6bc7685d4bc5370a0062
Suleman Khan <sulemank97@gmail.com>
2021-12-20 21:59:31 +0500

    set file limit to 15mb and throw error if file is not image

--- backend/src/middleware/upload-file.ts
+++ backend/src/middleware/upload-file.ts
@@ -2,18 +2,26 @@ import path from 'path';
 import { Request, Response, NextFunction } from 'express';
 import multer from 'multer';
 
-const allowedExtentions = ['.png', 'jpeg'];
+const allowedExtentions = ['.png', '.jpeg', '.jpg'];
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'public/uploads'),
-  filename: (req, file, cb) => cb(null, `${Date.now()}${file.originalname}`),
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${file.originalname}`);
+  },
 });
 const upload = multer({
   storage,
   limits: {
-    fieldSize: 100,
+    fieldSize: 1024 * 1024 * 15, // 15mb
+    fileSize: 1024 * 1024 * 15, // 15mb
   },
-  fileFilter: (req, file, cb) =>
-    cb(null, allowedExtentions.includes(path.extname(file.originalname))),
+  fileFilter: (req, file, cb) => {
+    if (!allowedExtentions.includes(path.extname(file.originalname))) {
+      cb(new Error('File extention is not supported'));
+    } else {
+      cb(null, true);
+    }
+  },
 });
 const multerUploadFile = upload.single('file');
 
